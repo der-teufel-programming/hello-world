1c087d948af779f0f4d18667ab3b4a22402a66c6
B1rtek <skrzeczek@onet.pl>
2021-04-19 09:28:25 +0200

    upgrejd

--- Android/app/src/main/java/com/birtek/heloword/MainActivity.kt
+++ Android/app/src/main/java/com/birtek/heloword/MainActivity.kt
@@ -2,10 +2,15 @@ package com.birtek.heloword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloButton.setOnClickListener {
+            textView.text = "Hi!"
+        }
     }
 }
