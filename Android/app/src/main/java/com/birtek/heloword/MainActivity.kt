60bf0d921a02a5e802e61879c05d3007389234bf
B1rtek <53182944+B1rtek@users.noreply.github.com>
2021-04-19 09:48:56 +0200

    Hello World App but upgraded and with correct OK

--- Android/app/src/main/java/com/birtek/heloword/MainActivity.kt
+++ Android/app/src/main/java/com/birtek/heloword/MainActivity.kt
@@ -3,15 +3,28 @@ package com.birtek.heloword
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+
+    var klikniecia = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         helloButton.setOnClickListener {
-            textView.text = "Hi!"
-            helloButton.text = "ok bye :)"
+            when (klikniecia) {
+                0 -> {
+                    textView.text = getString(R.string.haj)
+                    helloButton.text = getString(R.string.kthxbye)
+                }
+                1 -> {
+                    finish()
+                    exitProcess(0)
+                }
+            }
+            klikniecia++
         }
     }
 }
